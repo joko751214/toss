44884ce1cc1f3864e73bd81dfc0cdb84cb9b08d9
jin <jin_kys_t0808@ie8.com>
2023-11-10 17:59:25 +0800

    header 畫面切版

---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="layout">
+    <div class="header">
+      <div class="hamburger" @click.stop="store.openMenu()">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <Nav :class="store.open ? 'open' : ''" v-click-outside="close" />
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  import { useStatusStore } from '../stores/status';
+  import Nav from './Nav.vue';
+
+  const store = useStatusStore();
+
+  const openStatus = ref(false);
+
+  const close = item => {
+    if (!item) return store.closeMenu();
+  };
+</script>
+<style lang="scss">
+  .layout {
+    overflow: hidden;
+    position: relative;
+  }
+  .header {
+    display: flex;
+    justify-content: end;
+  }
+  .hamburger {
+    width: 50px;
+    padding: 5px 0;
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    background: #80808024;
+    border-radius: 5px;
+    border: 1px solid #000;
+    cursor: pointer;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    span {
+      display: block;
+      height: 2px;
+      width: 20px;
+      margin: 3px 0;
+      background: #000;
+      border-radius: 10px;
+    }
+  }
+</style>
+<script>
+  export default {
+    name: 'Header'
+  };
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="nav">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</template>
+<script setup></script>
+<style lang="scss" scoped>
+  .nav {
+    height: 100vh;
+    position: relative;
+    right: -10px;
+    background: #000;
+    color: #000;
+    width: 200px;
+    box-shadow: 0 1px 20px rgba(0, 0, 0, 0.25);
+    z-index: 1;
+    transform: translateX(380px);
+    opacity: 0;
+    transition: all 0.3s ease-in-out;
+    &.open {
+      transform: translateX(180px);
+      opacity: 1;
+    }
+    ul {
+      list-style: none;
+      padding: 0;
+
+      li {
+        width: 100%;
+        transition: all 0.15s ease;
+
+        a {
+          display: block;
+          padding: 20px;
+          width: 100%;
+          height: 100%;
+        }
+
+        &:hover {
+          background: grey;
+          color: yellow;
+          transition: all 0.15s ease;
+        }
+      }
+    }
+  }
+</style>
+<script>
+  export default {
+    name: 'Nav'
+  };
+</script>
