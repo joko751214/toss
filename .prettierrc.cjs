8177ab6ef071fef228e3ae99f53209ea5c296c28
jin <jin_kys_t0808@ie8.com>
2023-11-10 17:57:16 +0800

    建立自動排版工具、安裝 pinia 以及 sass

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  endOfLine: "auto",
+  printWidth: 100,
+  bracketSpacing: true,
+  vueIndentScriptAndStyle: true,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  proseWrap: "always",
+  quoteProps: "as-needed",
+};
